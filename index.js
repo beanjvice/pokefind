11282fa2e22158754c6985f819bfe8f8a006b338
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-23 13:07:55 -0400

    added initial setup

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import axios from 'axios'
+import bodyParser from 'body-parser'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const app = express()
+const port = 8000
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(filename)
+// middlewares
+app.use(express.static(path.join(__dirname+'public')))
+//app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+// routes
+app.get('/', (req, res) => {
+  // code for fetching all pokemons
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname,'views','index.html'))
+})
+app.post('/filter', (req, res) => {
+  // code for filtering pokemons
+})
+app.post('/:id', (req, res) => {
+  // code for fetchning pokemons by id
+})
+app.post('/save', (req, res) => {
+  // code for saving
+})
+app.listen(port, (req, res) => {
+  console.log(`Pokemon app is running successfully at port localhost:${port}`)
+})
