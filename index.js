ed93114ba82a164bbf84d75d509921648a443d6d
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-24 11:29:38 -0400

    work in progress of filter route

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ import bodyParser from 'body-parser'
 import path from 'path'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import https from 'https'
 const app = express()
 const port = 8000
 
 const filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(filename)
+
+const api = "https://pokeapi.co/api/v2"
 // middlewares
 app.use(express.static(path.join(__dirname + '/public')))
 app.use(express.static(path.join(__dirname + '/views')))
@@ -22,10 +25,20 @@ app.use(bodyParser.json())
 // routes
 app.get('/', (req, res) => {
   // code for fetching all pokemons
-  console.log(path.join(__dirname,'views','index.html'))
+  console.log(path.join(__dirname, 'views', 'index.html'))
 })
-app.post('/filter', (req, res) => {
+app.post('/filter', async (req, res) => {
+  console.log(req.body)
+  const { pokemon } = req.body;
+  try {
+    let response = await axios.get(`${api}/pokemon/${pokemon}`)
+    console.log('res', response)
+  }
+  catch (err) {
+    console.log('res', err)
+  }
   // code for filtering pokemons
+  res.redirect('/')
 })
 app.post('/:id', (req, res) => {
   // code for fetchning pokemons by id
