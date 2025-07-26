be19fa0ba319f5c48401b1429bffbf138537dd9b
Arisa Moonsammy <aricollegem@gmail.com>
2025-07-26 15:45:12 -0400

    commit

--- index.js
+++ index.js
@@ -20,42 +20,6 @@ app.use(bodyParser.json())
 // routes
 app.get('/', (req, res) => {
   // code for fetching all pokemons
-  console.log(pokemonList)
-  res.render("index.ejs", { data: pokemonList })
-})
-app.post('/filter', async (req, res) => {
-  let pokemonList = []
-  let nameList = []
-  console.log(req.body)
-  let { pokemon } = req.body;
-  pokemon = pokemon.split(" ").join("-")
-  let promises;
-  try {
-    let response = await axios.get(`${api}/ability/${pokemon}`)
-    nameList = response.data.pokemon
-  } catch (error) {
-    nameList.push(pokemon)
-  }
-  console.log(nameList)
-  if (nameList.length !== 0) {
-     promises = nameList.map(async (e) => {
-      try {
-        let response = await axios.get(`${api}/pokemon/${e.pokemon.name}`)
-        return response.data
-      }
-      catch (err) {
-        try{
-        let response = await axios.get(`${api}/pokemon/${pokemon}`)
-        return response.data
-        }
-        catch(error){
-             console.log(error)
-        }
-      }
-    }
-    )
-  }
- pokemonList = await Promise.all(promises)
   console.log(__dirname)
   res.sendFile(path.join(__dirname,'views','index.html'))
 })
